59f8db89240662ceac41c560c4999e65b0593997
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-06-10 17:02:24 +0200

    Esercizio completato

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,16 @@
 @extends( 'layout.app' )
 
 @section('movies')
-<main>
+<main class="d-flex justify-content-center align-items-center flex-wrap container-card">
 @foreach( $movies as $elem )
+<div class="card d-flex justify-content-center align-items-center">
     <h2>{{ $elem['title'] }}</h2>
+    <h3>lang: {{ $elem['nationality']}}</h3>
+    <h4>date: {{ $elem['date']}}</h4>
+    <h5>vote: {{ $elem['vote']}}</h5>
+
+
+</div>
 @endforeach
 </main>
 @endsection
